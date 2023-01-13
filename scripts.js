c421b1a44c10b46a2df4f68976951b00de54cc75
jpfairhart <johnpaulfairhart@hotmail.com>
2023-01-12 21:37:27 -0800

    add numOfPages method to Book object

--- scripts.js
+++ scripts.js
@@ -13,6 +13,9 @@ function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
+  this.numOfPages = function() {
+    return `Pages: ${pages}`;
+  };
   this.read = read
   this.readStatus = function() {
     let readOrNot = "Read: No";
